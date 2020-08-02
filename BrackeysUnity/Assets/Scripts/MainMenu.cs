bc24e843f0dba9f220d6dc365f33654be48101b8
LauriceThomas <49695257+LauriceThomas@users.noreply.github.com>
2020-08-02 17:35:45 -0500

    Added Base Menus
    All UI buttons are hooked up and pause works.
    Still needs UI Art and a discussion over which settings need to be programmed.

---
+++ BrackeysUnity/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+public class MainMenu : MonoBehaviour
+{
+    public Canvas StartCanvas;
+    public Canvas ControlsCanvas;
+    public Canvas SettingsCanvas;
+    public Canvas InstructionsCanvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCanvas.enabled = true;
+        ControlsCanvas.enabled = false;
+        SettingsCanvas.enabled = false;
+        InstructionsCanvas.enabled = false;
+    }
+
+    public void PressedStart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void PressedControls()
+    {
+        StartCanvas.enabled = false;
+        ControlsCanvas.enabled = true;
+        SettingsCanvas.enabled = false;
+        InstructionsCanvas.enabled = false;
+    }
+
+    public void PressedInstructions()
+    {
+        StartCanvas.enabled = false;
+        ControlsCanvas.enabled = false;
+        SettingsCanvas.enabled = false;
+        InstructionsCanvas.enabled = true;
+    }
+
+    public void PressedSettings()
+    {
+        StartCanvas.enabled = false;
+        ControlsCanvas.enabled = false;
+        SettingsCanvas.enabled = true;
+        InstructionsCanvas.enabled = false;
+    }
+
+    public void PressedBack()
+    {
+        StartCanvas.enabled = true;
+        ControlsCanvas.enabled = false;
+        SettingsCanvas.enabled = false;
+        InstructionsCanvas.enabled = false;
+    }
+
+    public void PressedQuit()
+    {
+#if UNITY_EDITOR
+        EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+    }
+
+}
---
+++ BrackeysUnity/Assets/Scripts/PauseManu.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+public class PauseManu : MonoBehaviour
+{
+    public Canvas PauseCanvas;
+    public Canvas ControlsCanvas;
+    public Canvas SettingsCanvas;
+    public Canvas InstructionsCanvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PauseCanvas.enabled = false;
+        ControlsCanvas.enabled = false;
+        SettingsCanvas.enabled = false;
+        InstructionsCanvas.enabled = false;
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(Time.timeScale == 1)
+            {
+                PauseGame();
+            }
+
+            else
+            {
+                ResumeGame();
+            }
+        }
+    }
+
+    public void ResumeGame()
+    {
+        PauseCanvas.enabled = false;
+        ControlsCanvas.enabled = false;
+        SettingsCanvas.enabled = false;
+        InstructionsCanvas.enabled = false;
+        Time.timeScale = 1;
+    }
+
+    public void PauseGame()
+    {
+        PauseCanvas.enabled = true;
+        ControlsCanvas.enabled = false;
+        SettingsCanvas.enabled = false;
+        InstructionsCanvas.enabled = false;
+        Time.timeScale = 0;
+    }
+
+    public void PressedControls()
+    {
+        PauseCanvas.enabled = false;
+        ControlsCanvas.enabled = true;
+        SettingsCanvas.enabled = false;
+        InstructionsCanvas.enabled = false;
+    }
+
+    public void PressedInstructions()
+    {
+        PauseCanvas.enabled = false;
+        ControlsCanvas.enabled = false;
+        SettingsCanvas.enabled = false;
+        InstructionsCanvas.enabled = true;
+    }
+
+    public void PressedSettings()
+    {
+        PauseCanvas.enabled = false;
+        ControlsCanvas.enabled = false;
+        SettingsCanvas.enabled = true;
+        InstructionsCanvas.enabled = false;
+    }
+
+    public void PressedBack()
+    {
+        PauseCanvas.enabled = true;
+        ControlsCanvas.enabled = false;
+        SettingsCanvas.enabled = false;
+        InstructionsCanvas.enabled = false;
+    }
+
+    public void PressedQuit()
+    {
+#if UNITY_EDITOR
+        EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+    }
+}
