df4809ab223c1a2a1a08ea8f534de569e060467f
Joaquin Ramirez <joaquinramirez97@yahoo.com>
2020-08-02 02:44:23 -0500

    Customer movement an Spawner basis

---
+++ BrackeysUnity/Assets/TaveraTest/Scripts/CustomerMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomerMovement : MonoBehaviour
+{
+    public float speed = 4;
+    public Transform destination;
+    
+    [HideInInspector]
+    public int spotIndex;
+    
+    private bool isMovingHorizontally = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!destination) { return; }
+
+        if (Vector2.Distance(transform.position, destination.position) <= 0) { return; }
+
+        isMovingHorizontally = transform.position.x != destination.position.x;
+
+        if(isMovingHorizontally)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, 
+                new Vector2(destination.position.x, transform.position.y), speed * Time.deltaTime);
+        }
+        else
+        {
+            transform.position = Vector2.MoveTowards(transform.position,
+                new Vector2(transform.position.x, destination.position.y), speed * Time.deltaTime);
+        }
+    }
+}
---
+++ BrackeysUnity/Assets/TaveraTest/Scripts/CustomerSpawner.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomerSpawner : MonoBehaviour
+{
+    public List<GameObject> customers;
+    public float spawnRate = 4.0f;
+    public GameObject customerPrefab;
+    public GameObject[] lineSpots;
+
+    private float spawnTimer;
+
+    private void Awake()
+    {
+        // TODO Move this line to a manager
+        Physics2D.gravity = new Vector2(0, 0);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        customers = new List<GameObject>();
+        spawnTimer = spawnRate;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(spawnTimer <= 0 && customers.Count < 3)
+        {
+            GameObject newCustomer = Instantiate(customerPrefab, transform.position, transform.rotation);
+            customers.Add(newCustomer);
+
+            newCustomer.GetComponent<CustomerMovement>().destination = lineSpots[customers.Count - 1].transform;
+
+            spawnTimer = spawnRate;
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space) && customers.Count > 0)
+        {
+            customers[0].GetComponent<CustomerMovement>().destination = transform;
+            Destroy(customers[0], 4f);
+
+            spawnTimer = spawnRate;
+
+            customers.RemoveAt(0);
+            UpdateCustomerSpots();
+        }
+
+        spawnTimer -= Time.deltaTime;
+    }
+
+    void UpdateCustomerSpots()
+    {
+        for(int i = 0; i < customers.Count; i++)
+        {
+            customers[i].GetComponent<CustomerMovement>().destination = lineSpots[i].transform;
+        }
+    }
+}
