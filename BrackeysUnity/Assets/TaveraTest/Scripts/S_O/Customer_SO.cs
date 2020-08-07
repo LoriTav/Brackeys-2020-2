238f95b0751751176435dfc325f01854abc8faf6
Joaquin Ramirez <joaquinramirez97@yahoo.com>
2020-08-07 18:30:50 -0500

    Created scriptable objetcs for customer variations

---
+++ BrackeysUnity/Assets/TaveraTest/Scripts/S_O/Customer_SO.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Customer", menuName = "Customer/New Animal")]
+public class Customer_SO : ScriptableObject
+{
+    public Sprite frontView;
+    public Sprite backView;
+    public Sprite sideView;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
