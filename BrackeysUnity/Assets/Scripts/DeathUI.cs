1be48403ab9402e1aafe88b9a890df6ba4a74394
LauriceThomas <49695257+LauriceThomas@users.noreply.github.com>
2020-08-06 23:05:57 -0500

    Game over works

---
+++ BrackeysUnity/Assets/Scripts/DeathUI.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DeathUI : MonoBehaviour
+{
+    public Text PayText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PayText.text = string.Format("But at least you earned $" + ScoreManager.instance.score.ToString() + " first!");
+    }
+}
--- BrackeysUnity/Assets/TaveraTest/Scripts/Managers/GameManager.cs
+++ BrackeysUnity/Assets/TaveraTest/Scripts/Managers/GameManager.cs
@@ -1,12 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
     public bool IsGameOver;
     public bool IsAttendingCustomer;
+    public float DeathTimer;
 
     private void Awake()
     {
@@ -31,12 +32,28 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(IsGameOver == true)
+        {
+            if (SceneManager.GetActiveScene().name != "GameOver")
+            {
+                SceneManager.LoadScene("GameOver");
+            }
+
+            DeathTimer -= Time.deltaTime;
+        }
+
+        if(DeathTimer <= 0f && IsGameOver == true)
+        {
+            ScoreManager.instance.ResetScoreManager();
+            SceneManager.LoadScene(0);
+            ResetGameManager();
+        }
     }
 
     public void ResetGameManager()
     {
         IsGameOver = false;
         IsAttendingCustomer = false;
+        DeathTimer = 5.0f;
     }
 }
